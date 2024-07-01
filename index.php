d50785642392148f6f47d2261caaa2296dcd0d0a
Meison <69295331+Meison12345@users.noreply.github.com>
2024-07-01 17:38:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <title></title>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <link href="/style/normalize.css" rel="stylesheet" type="text/css" />
+   <link href="/style/style.css" rel="stylesheet" type="text/css" />
+    <link href="/style/effect.css" rel="stylesheet" type="text/css" />
+   <title>Галактический вестник</title>
+ </head>
+ <body>
+
+
+
+
+<header class="center header">
+	<nav class="header-nav">
+		<a href="index.php">
+			<img src="/img/logo.svg" alt="Лого сайтма" width="390" height="74">
+		</a>
+		
+		<a href="#">Главная</a>
+	</nav>
+</header>
+
+
+<!-- <section class="main-news">
+	<div class="main-news-wrapper">
+		<h1 class="main-news--h1">Возвращение этнографа</h1>
+		<h2 class="main-news--h2">Сегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</h2>
+	</div>
+</section>
+ -->
+
+<main class="center news">
+	<h3 class="news-title">Новости</h3>
+
+<div class="news--item-wrapper">
+	<div class="news--item">
+		<p class="news-item-time">11.06.2412</p>
+		<p class="news--item-title">Возвращение этнографа</p>
+		<p class="news--item-text">Cегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</p>
+		<a href="#" class="news--item-btn">Подробнее<img src="/img/arrow.svg" alt="Стрелка" width="" height=""></a>
+	</div>
+
+	<div class="news--item">
+		<p class="news-item-time">11.06.2412</p>
+		<p class="news--item-title">Несчастный случай с известной светской дивой</p>
+		<p class="news--item-text">Светская дива Алиса Уткина попала под троллейбус, пытаясь уйти от преследования.</p>
+		<a href="#" class="news--item-btn">Подробнее<img src="/img/arrow.svg" alt="Стрелка" width="" height=""></a>
+	</div>
+
+	<div class="news--item">
+		<p class="news-item-time">26.05.2412</p>
+		<p class="news--item-title">Папа Римский совершил визит в систему Альфы Центавра</p>
+		<p class="news--item-text">Папа Римский Жан-Клод XIV посетил с апостольским визитом систему Альфы Центавра. Он принял участие в ряде благотворительных мероприятий и совершил богослужение в соборе Ван-Дамм-де-Ури.</p>
+		<a href="#" class="news--item-btn">Подробнее<img src="/img/arrow.svg" alt="Стрелка" width="" height=""></a>
+	</div>
+
+	<div class="news--item">
+		<p class="news-item-time">20.05.2412</p>
+		<p class="news--item-title">На чемпионате по пустотной гребле победила команда с Фобоса</p>
+		<p class="news--item-text">В поясе астероидов на проходившем в последние выходные этапе системного чемпионата по пустотной гребле команда с Фобоса одержала 
+убедительную победу.</p>
+		<a href="#" class="news--item-btn">Подробнее<img src="/img/arrow.svg" alt="Стрелка" width="" height=""></a>
+	</div>
+</div>
+</main>
+
+
+<nav class="prefooter-nav center">
+	<ul class="prefooter-nav-list">
+		<li class="prefooter-nav-list__element"><a href="#" class="prefooter-nav-list__element-link">1</a></li>
+		<li class="prefooter-nav-list__element"><a href="#" class="prefooter-nav-list__element-link">2</a></li>
+		<li class="prefooter-nav-list__element"><a href="#" class="prefooter-nav-list__element-link">3</a></li>
+		<li class="prefooter-nav-list__element"><a href="#" class="prefooter-nav-list__element-link">></a></li>
+	</ul>
+</nav>
+
+
+<div class="line center"></div>
+
+
+<footer class="footer center">
+	<p class="footer-text">© 2023 — 2412 «Галактический вестник»</p>
+</footer>
+
+ </body> 
+</html>
